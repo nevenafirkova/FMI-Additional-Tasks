77d0bedc28ea25d6dbf0df7d535e30869f0cbbe3
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-23 12:23:02 +0200

    Add files via upload

---
+++ 01. First Year/01. Task 1 - Reversed Numbers/Task1-Version2.cs
@@ -0,0 +1,15 @@
+﻿//INPUT
+int inputNumber = int.Parse(Console.ReadLine());
+
+//CHANGEABLE
+int currentNumber = inputNumber;
+
+//ACTION
+while (currentNumber > 0)
+{
+    int lastDigit = currentNumber % 10;
+
+    Console.WriteLine(lastDigit);
+
+    currentNumber = (currentNumber - lastDigit) / 10;
+}
