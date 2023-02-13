a8fab32e43884cb6f2006837c140169b55d57606
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-14 00:20:05 +0200

    Add files via upload

---
+++ 01. First Year/04. Second Biggest Array Number/04.SecondBiggestArrayNumber.cs
@@ -0,0 +1,22 @@
+﻿//INPUT
+int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//ACTION
+int biggestNumber = int.MinValue;
+int secondBiggestNumber = int.MinValue;
+
+foreach (int number in inputArray)
+{
+    if (number > biggestNumber)
+    {
+        secondBiggestNumber = biggestNumber;
+        biggestNumber = number;
+    }
+    else if (number > secondBiggestNumber)
+    {
+        secondBiggestNumber = number;
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"The second biggest number is: {secondBiggestNumber}.");
